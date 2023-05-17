ed7ce531e2c533687046c598d5812ebad8de2784
exit <halimshams8@gmail.com>
2023-05-17 18:53:07 +0430

    responsiveness added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
 		'./app/javascript/**/*.js',
 	],
 	theme: {
+		screens: {
+			'sm': '640px',
+      		'md': '1070px',
+      		'lg': '1215px',
+      		'xl': '1280px',
+      		'2xl': '1536px',
+		},
 		extend: {
 			fontFamily: {
 				montserrat: ['Montserrat', 'sans-serif'],
